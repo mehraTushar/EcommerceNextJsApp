ba3c28109b458a37a3157871633a748cdb4cfb58
Tushar Mehra <70595562+mehraTushar@users.noreply.github.com>
2024-09-27 07:40:29 +0530

    Update Navbar.jsx

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import MainNav from "./mainNav";
-import MobileNav from "./mobileNav";
+import MainNav from "./MainNav";
+import MobileNav from "./MobileNav";
 
 export default function NavBar() {
   return (
